9301210afc04dc8683638cfe327dd186b802bf7b
BuildTools <unconfigured@null.spigotmc.org>
2017-12-26 11:53:08 +0000

    Create login and main activity

--- app/src/main/java/uk/co/portalcomputing/giftsync/HomePage.java
+++ app/src/main/java/uk/co/portalcomputing/giftsync/HomePage.java
@@ -1,12 +1,25 @@
 package uk.co.portalcomputing.giftsync;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomePage extends AppCompatActivity {
-//testing github
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("Account",MODE_PRIVATE);
+
+        if(sharedPreferences.getBoolean("LoggedIn",false)){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, LoginOrReg.class);
+            startActivity(intent);
+        }
+        finish();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
---
+++ app/src/main/java/uk/co/portalcomputing/giftsync/LoginOrReg.java
@@ -0,0 +1,13 @@
+package uk.co.portalcomputing.giftsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginOrReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_reg);
+    }
+}
